3cee9cca7e5eaddb7c73a1103f2738d2e6760ec0
Your Name <you@example.com>
2024-02-22 03:57:41 +0100

    ItemsManager optimization
    I made some optimization for ItemsManager. For instance, there's no need to check for time in every frame.

--- Assets/Scripts/Items/ItemsManager.cs
+++ Assets/Scripts/Items/ItemsManager.cs
@@ -1,57 +1,79 @@
 ﻿namespace AFSInterview.Items
 {
-	using TMPro;
-	using UnityEngine;
+    using TMPro;
+    using UnityEngine;
 
-	public class ItemsManager : MonoBehaviour
-	{
-		[SerializeField] private InventoryController inventoryController;
-		[SerializeField] private int itemSellMaxValue;
-		[SerializeField] private Transform itemSpawnParent;
-		[SerializeField] private GameObject itemPrefab;
-		[SerializeField] private BoxCollider itemSpawnArea;
-		[SerializeField] private float itemSpawnInterval;
+    public class ItemsManager : MonoBehaviour
+    {
+        [SerializeField] private InventoryController inventoryController;
+        [SerializeField] private int itemSellMaxValue;
+        [SerializeField] private Transform itemSpawnParent;
+        [SerializeField] private GameObject itemPrefab;
+        [SerializeField] private BoxCollider itemSpawnArea;
+        [SerializeField] private float itemSpawnInterval;
+        [SerializeField] private TextMeshProUGUI moneyText;
 
-		private float nextItemSpawnTime;
-		
-		private void Update()
-		{
-			if (Time.time >= nextItemSpawnTime)
-				SpawnNewItem();
-			
-			if (Input.GetMouseButtonDown(0))
-				TryPickUpItem();
-			
-			if (Input.GetKeyDown(KeyCode.Space))
-				inventoryController.SellAllItemsUpToValue(itemSellMaxValue);
+        private Camera cameraMain;
+        private int layerMask;
 
-			FindObjectOfType<TextMeshProUGUI>().text = "Money: " + inventoryController.Money;
-		}
+        private void Start()
+        {
+            cameraMain = Camera.main;
+            layerMask = LayerMask.GetMask("Item");
 
-		private void SpawnNewItem()
-		{
-			nextItemSpawnTime = Time.time + itemSpawnInterval;
-			
-			var spawnAreaBounds = itemSpawnArea.bounds;
-			var position = new Vector3(
-				Random.Range(spawnAreaBounds.min.x, spawnAreaBounds.max.x),
-				0f,
-				Random.Range(spawnAreaBounds.min.z, spawnAreaBounds.max.z)
-			);
-			
-			Instantiate(itemPrefab, position, Quaternion.identity, itemSpawnParent);
-		}
+            UpdateMoneyText();
 
-		private void TryPickUpItem()
-		{
-			var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			var layerMask = LayerMask.GetMask("Item");
-			if (!Physics.Raycast(ray, out var hit, 100f, layerMask) || !hit.collider.TryGetComponent<IItemHolder>(out var itemHolder))
-				return;
-			
-			var item = itemHolder.GetItem(true);
-            inventoryController.AddItem(item);
-            Debug.Log("Picked up " + item.Name + " with value of " + item.Value + " and now have " + inventoryController.ItemsCount + " items");
-		}
-	}
+            InvokeRepeating(nameof(SpawnNewItem), 0f, itemSpawnInterval);
+        }
+
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+                TryPickUpItem();
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                inventoryController.SellAllItemsUpToValue(itemSellMaxValue);
+                UpdateMoneyText();
+            }
+        }
+
+        private void UpdateMoneyText()
+        {
+            moneyText.text = "Money: " + inventoryController.Money;
+        }
+
+        private void SpawnNewItem()
+        {
+            var spawnAreaBounds = itemSpawnArea.bounds;
+            var position = new Vector3(
+                Random.Range(spawnAreaBounds.min.x, spawnAreaBounds.max.x),
+                0f,
+                Random.Range(spawnAreaBounds.min.z, spawnAreaBounds.max.z)
+            );
+
+            Instantiate(itemPrefab, position, Quaternion.identity, itemSpawnParent);
+        }
+
+        private void TryPickUpItem()
+        {
+            if (GetItemHolderOnPosition(Input.mousePosition, out IItemHolder itemHolder))
+            {
+                var item = itemHolder.GetItem(true);
+                inventoryController.AddItem(item);
+                Debug.Log("Picked up " + item.Name + " with value of " + item.Value + " and now have " + inventoryController.ItemsCount + " items");
+            }
+        }
+
+        private bool GetItemHolderOnPosition(Vector3 position, out IItemHolder itemHolder)
+        {
+            itemHolder = null;
+
+            var ray = cameraMain.ScreenPointToRay(position);
+            if (!Physics.Raycast(ray, out var hit, 100f, layerMask) || !hit.transform.gameObject.TryGetComponent<IItemHolder>(out itemHolder))
+                return false;
+
+            return true;
+        }
+    }
 }
